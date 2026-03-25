5ae0b8fdbd2db0eafa283b978e2fe6a972f1357b
David Arias <davariasc@udistrital.edu.co>
2026-03-25 07:44:02 -0500

    feat: Se ajusta función de busqueda de solicitud por identificacion, trae el estado de la solicitud y al crear ya se crean los documentos udistrital/sga_documentacion_inversion#120

--- services/solicitud.go
+++ services/solicitud.go
@@ -50,10 +50,16 @@ func CrearSolicitud(solicitud models.CrearSolicitudEntrada) (respuesta models.So
 									Formulario:  string(formularioBytes),
 									Activo:      true,
 								}
+								var id_estado int
+								if solicitud.Formulario["formulario_completado"] == false || len(solicitud.DocumentoSolicitud) == 0 || solicitud.Observacion == ""{
+									id_estado = 1
+								}else{
+									id_estado = 2
+								}
 
 								historico_solicitud := models.HistoricoEstadoSolicitud{
 									SolicitudId:       &solicitud_temp,
-									EstadoSolicitudId: &models.EstadoSolicitud{Id: 2},
+									EstadoSolicitudId: &models.EstadoSolicitud{Id: id_estado},
 									RolUsuario:        solicitud.CodigoAbreviacionRol,
 									TerceroId:         int(id_tercero),
 									Activo:            true,
@@ -64,9 +70,49 @@ func CrearSolicitud(solicitud models.CrearSolicitudEntrada) (respuesta models.So
 								fmt.Println("CREO LA SOLICITUD")
 								if err := request.SendJson(beego.AppConfig.String("UrlComisionesCrud")+"detalle_solicitud", "POST", &respuesta_detalle_solicitud, &detalles_solicitud); err == nil {
 									fmt.Println("SE CREA LA SOLICITUD CON FORMULARIO")
-								}
-								if err := request.SendJson(beego.AppConfig.String("UrlComisionesCrud")+"historico_estado_solicitud", "POST", &respuesta_historico_estado_solicitud, &historico_solicitud); err == nil {
-									fmt.Println("SE CREA EL HISTORICO")
+									if err := request.SendJson(beego.AppConfig.String("UrlComisionesCrud")+"historico_estado_solicitud", "POST", &respuesta_historico_estado_solicitud, &historico_solicitud); err == nil {
+										if solicitud.Formulario["formulario_completado"] == false || len(solicitud.DocumentoSolicitud) == 0 || solicitud.Observacion == ""{
+											if data_estado_historico, ok := respuesta_historico_estado_solicitud["Data"].(map[string]interface{}); ok {
+												var id_historico_estado int
+												if id, ok := data_estado_historico["Id"].(float64); ok {
+													id_historico_estado = int(id)
+												}
+												if len(solicitud.DocumentoSolicitud) != 0{
+													var resultado_documentos []map[string]interface{}
+													var errDoc map[string]interface{}
+													if resultado_documentos,errDoc  = helpers.CrearDocumento(solicitud.DocumentoSolicitud); errDoc == nil{
+														for _, doc := range resultado_documentos {
+															fmt.Println("DOCUMENTOS CREADOS")
+															fmt.Println(doc)
+															var idDoc int
+															switch v := doc["id"].(type) {
+															case float64:
+																idDoc = int(v)
+															case int:
+																idDoc = v
+															default:
+																fmt.Println("ERROR: tipo inesperado en id", v)
+																continue
+															}
+															documento_solicitud := models.DocumentoSolicitud{
+																DocumentoId: idDoc,
+																SolicitudEstadoEvento:	&models.HistoricoEstadoSolicitud{Id: id_historico_estado},
+																TipoDocumento:			&models.TipoDocumentoSolicitud{Id: 1},
+																EstadoDocumento:		&models.EstadoDocumento{Id: 1},
+																Activo:					true,
+															}
+															var respuesta_documento_solicitud map[string]interface{}
+															if err := request.SendJson(beego.AppConfig.String("UrlComisionesCrud")+"documento_solicitud", "POST", &respuesta_documento_solicitud, &documento_solicitud); err == nil {
+																fmt.Println("documento creado y anexado a la solicitud")
+															}
+
+														}
+													}
+												} 
+											}
+										}
+										fmt.Println("SE CREA EL HISTORICO")
+									}
 								}
 							}
 						}
@@ -92,50 +138,94 @@ func BuscarSolicitudIdentificacion(identificacion int) (respuesta []models.Solic
 		}
 	}()
 
+	//Busca el tercero
 	var persona map[string]interface{}
+	var tercero []map[string]interface{}
+	if err := request.GetJson(beego.AppConfig.String("UrlTercerosCrud")+"datos_identificacion?query=Numero:"+fmt.Sprintf("%d", identificacion), &tercero); err == nil {
+		if len(tercero) > 0 && len(tercero[0]) > 0 {
+			if tercero_comprobacion, ok := tercero[0]["TerceroId"].(map[string]interface{}); ok {
+				if id_tercero, ok := tercero_comprobacion["Id"].(float64); ok {
+					id_tercero_busqueda :=int(id_tercero)
 
-	fmt.Println("ENTRA A SERVICIO ", beego.AppConfig.String("UrlComisionesCrud")+"solicitud?query=TerceroId:"+fmt.Sprintf("%d", identificacion))
-	if err := request.GetJson(
-		beego.AppConfig.String("UrlComisionesCrud")+"solicitud?query=TerceroId:"+fmt.Sprintf("%d", identificacion),
-		&persona,
-	); err == nil {
-		fmt.Println("ENTRA A SERVICIO 2 ", persona)
-		if data, ok := persona["Data"].([]interface{}); ok && len(data) > 0 {
-			for _, item := range data {
-				var detalleSolicitud map[string]interface{}
+					fmt.Println("ENTRA A SERVICIO ", beego.AppConfig.String("UrlComisionesCrud")+"solicitud?query=TerceroId:"+fmt.Sprintf("%d", id_tercero_busqueda)+"&limit=-1")
+					if err := request.GetJson(beego.AppConfig.String("UrlComisionesCrud")+"solicitud?query=TerceroId:"+fmt.Sprintf("%d", id_tercero_busqueda),
+						&persona,); err == nil {
+						fmt.Println("ENTRA A SERVICIO 2 ", persona)
+						if data, ok := persona["Data"].([]interface{}); ok && len(data) > 0 {
+							for _, item := range data {
+								var detalleSolicitud map[string]interface{}
 
-				if itemMap, ok := item.(map[string]interface{}); ok {
-					var sol models.SolicitudResumen
-					idStr := fmt.Sprintf("%v", itemMap["Id"])
-					fmt.Println("ID SOLICITUD ", idStr)
-					/*if err := request.GetJson(
-						beego.AppConfig.String("UrlComisionesCrud")+"historial_solicitud?query=SolicitudId:"+idStr,
-						&detalleSolicitud,
-					)*/
-					if err := request.GetJson(
-						beego.AppConfig.String("UrlComisionesCrud")+"detalle_solicitud?query=solicitud_id:"+idStr,
-						&detalleSolicitud,
-					); err == nil {
-						fmt.Println("DETALLE SOLICITUD ", detalleSolicitud)
-						datosFormulario, err := helpers.ObtenerDatosFormulario(detalleSolicitud)
-						if err == nil {
-							fmt.Println("Programa: ", datosFormulario.Solicitante.Q7Proyecto)
-							fmt.Println("Nombre: ", datosFormulario.Solicitante.Q3NombresApellidos)
-							if id, ok := itemMap["Id"].(float64); ok {
-								sol.Id = int(id)
+								if itemMap, ok := item.(map[string]interface{}); ok {
+									var sol models.SolicitudResumen
+									idStr := fmt.Sprintf("%v", itemMap["Id"])
+									fmt.Println("ID SOLICITUD ", idStr)
+									/*if err := request.GetJson(
+										beego.AppConfig.String("UrlComisionesCrud")+"historial_solicitud?query=SolicitudId:"+idStr,
+										&detalleSolicitud,
+									)*/
+									sol.FechaCreacion = fmt.Sprintf("%v", itemMap["FechaCreacion"])
+									if err := request.GetJson(
+										beego.AppConfig.String("UrlComisionesCrud")+"detalle_solicitud?query=solicitud_id:"+idStr,
+										&detalleSolicitud,
+									); err == nil {
+										fmt.Println("DETALLE SOLICITUD ", detalleSolicitud)
+										datosFormulario, err := helpers.ObtenerDatosFormulario(detalleSolicitud)
+										if err == nil {
+											fmt.Println("Programa: ", datosFormulario.Solicitante.Q7Proyecto)
+											fmt.Println("Nombre: ", datosFormulario.Solicitante.Q3NombresApellidos)
+											if id, ok := itemMap["Id"].(float64); ok {
+												sol.Id = int(id)
+											}
+											if activo, ok := itemMap["Activo"].(bool); ok {
+												sol.Activo = activo
+											}
+											sol.Programa = datosFormulario.Solicitante.Q7Proyecto
+											sol.Nombre = datosFormulario.Solicitante.Q3NombresApellidos
+										}
+										var respuesta_historico_estado_solicitud_actual map[string]interface{}
+										var id_estado_solicitud int
+										if err := request.GetJson(
+											beego.AppConfig.String("UrlComisionesCrud")+"historico_estado_solicitud?query=solicitudId__Id:"+idStr+",Activo:true&sortby=FechaCreacion&order=desc&limit=1",
+											&respuesta_historico_estado_solicitud_actual,
+										); err == nil {
+											if data, ok := respuesta_historico_estado_solicitud_actual["Data"].([]interface{}); ok && len(data) > 0 {
+
+												if primerRegistro, ok := data[0].(map[string]interface{}); ok {
+													if estado, ok := primerRegistro["EstadoSolicitudId"].(map[string]interface{}); ok {
+														switch v := estado["Id"].(type) {
+														case float64:
+															id_estado_solicitud = int(v)
+														case int:
+															id_estado_solicitud = v
+														default:
+															fmt.Println("Tipo inesperado en Id")
+														}
+
+														nombreEstado, ok := estado["Nombre"].(string)
+														if !ok {
+															fmt.Println("Nombre no válido")
+															nombreEstado = "" // o algún valor por defecto
+														}
+
+
+														sol.EstadoSolicitud = &models.EstadoSolicitud{
+															Id: id_estado_solicitud,
+															Nombre: nombreEstado,
+														}
+													}
+												}
+											}
+										}
+									}
+									respuesta = append(respuesta, sol)
+								}
 							}
-							if activo, ok := itemMap["Activo"].(bool); ok {
-								sol.Activo = activo
-							}
-							sol.Programa = datosFormulario.Solicitante.Q7Proyecto
-							sol.Nombre = datosFormulario.Solicitante.Q3NombresApellidos
+
+							return respuesta, nil
 						}
 					}
-					respuesta = append(respuesta, sol)
 				}
 			}
-
-			return respuesta, nil
 		}
 	}
 
