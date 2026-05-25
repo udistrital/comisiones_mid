d740e108c0a66a1f3534c6cc84b595a50e3b7deb
diagutierrezro <diagutierrezro@unal.edu.co>
2026-05-25 08:43:09 -0500

    fix: Ajuste de swagger

--- models/historico_estado_solicitud.go
+++ models/historico_estado_solicitud.go
@@ -36,24 +36,3 @@ type ResponseHistoricoSolicitudProrroga struct {
 	Message string                       `json:"Message"`
 	Data    []HistoricoSolicitudProrroga `json:"Data"`
 }
-
-type ResponseListaHistoricoEstadoSolicitud struct {
-	Data    []HistoricoEstadoSolicitud `json:"Data"`
-	Message string                     `json:"Message"`
-	Status  string                     `json:"Status"`
-	Success bool                       `json:"Success"`
-}
-
-type HistoricoSolicitudProrroga struct {
-	SolicitudId   int    `json:"solicitud_id"`
-	HistoricoId   int    `json:"historico_id"`
-	FechaCreacion string `json:"fecha_creacion"`
-	Estado        string `json:"estado"`
-}
-
-type ResponseHistoricoSolicitudProrroga struct {
-	Success bool                         `json:"Success"`
-	Status  string                       `json:"Status"`
-	Message string                       `json:"Message"`
-	Data    []HistoricoSolicitudProrroga `json:"Data"`
-}
